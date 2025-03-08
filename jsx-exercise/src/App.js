f0b1ddd710ae3b3a035de72e751a2e0faf3c601d
Arsalan <arsalan20044@gmail.com>
2025-03-07 21:36:22 -0700

    Exercise 2

--- jsx-exercise/src/App.js
+++ jsx-exercise/src/App.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import Home from './Home';
+import About from './About';
+import Contact from './Contact';
 
 function App() {
   const currentYear = new Date().getFullYear();
@@ -10,6 +13,10 @@ function App() {
       <p>React Components</p>
       <p>Current Year: {currentYear}</p>
       <p>{isLoggedin ? "Welcome back!" : "Please log in."}</p>
+
+      <Home title="Home Page" description="Welcome to our website." />
+      <About title="About Us" description="We are passionate about delivering quality experiences." />
+      <Contact title="Contact Us" description="Feel free to reach out to us via email or phone." />
     </div>
   );
 }
